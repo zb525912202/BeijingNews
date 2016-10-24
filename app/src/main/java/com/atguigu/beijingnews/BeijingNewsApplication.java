3171b02d73cebe0d40438efb62e454f11065eebc
ganchuanpu <429533813@qq.com>
2016-10-24 20:24:05 +0800

    Update BeijingNewsApplication.java

--- app/src/main/java/com/atguigu/beijingnews/BeijingNewsApplication.java
+++ app/src/main/java/com/atguigu/beijingnews/BeijingNewsApplication.java
@@ -14,9 +14,6 @@ import org.xutils.x;
 import cn.jpush.android.api.JPushInterface;
 
 /**
- * 作者：尚硅谷-杨光福 on 2016/8/15 09:16
- * 微信：yangguangfu520
- * QQ号：541433511
  * 作用：代表整个软件
  */
 public class BeijingNewsApplication extends Application {
